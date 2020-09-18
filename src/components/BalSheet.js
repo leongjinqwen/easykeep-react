82fb603e8bd24892020655c4ca8793d843947bcd
leongjinqwen <leongjinqwen@hotmail.com>
2020-09-18 20:37:06 +0800

    code clean up

--- src/components/BalSheet.js
+++ src/components/BalSheet.js
@@ -25,8 +25,7 @@ export default function BalSheet({ accounts, assess }) {
     accs.map(a=>{
       return(
         <TableRow key={a.account_number}>
-          <TableCell component="th" scope="row">{a.account_number}</TableCell>
-          <TableCell>{a.account_name}</TableCell>
+          <TableCell component="th" scope="row">{a.account_number} - {a.account_name}</TableCell>
           <TableCell align="right">{(a.amount).toFixed(2)}</TableCell>
         </TableRow>
       )
@@ -43,7 +42,6 @@ export default function BalSheet({ accounts, assess }) {
           <TableHead>
             <TableRow>
               <TableCell></TableCell>
-              <TableCell></TableCell>
               <TableCell align="right"><strong>Amount ($)</strong></TableCell>
             </TableRow>
           </TableHead>
@@ -51,30 +49,30 @@ export default function BalSheet({ accounts, assess }) {
             { accounts ? renderAccounts(accounts.f_assets.list) : null }
             { accounts ? renderAccounts(accounts.c_assets.list) : null }
             <TableRow className={classes.bold}>
-              <TableCell component="th" scope="row">Total ASSETS</TableCell>
-              <TableCell></TableCell>
-              <TableCell align="right">{(accounts.f_assets.total+accounts.c_assets.total).toFixed(2)}</TableCell>
+              <TableCell component="th" scope="row"><strong>Total ASSETS</strong></TableCell>
+              <TableCell align="right"><strong>{(accounts.f_assets.total+accounts.c_assets.total).toFixed(2)}</strong></TableCell>
             </TableRow>
 
             { accounts ? renderAccounts(accounts.n_c_liabilities.list) : null }
             { accounts ? renderAccounts(accounts.c_liabilities.list) : null }
             <TableRow className={classes.bold}>
               <TableCell component="th" scope="row">Total LIABILITIES</TableCell>
-              <TableCell></TableCell>
               <TableCell align="right">{(-(accounts.n_c_liabilities.total+accounts.c_liabilities.total)).toFixed(2)}</TableCell>
             </TableRow>
 
             { accounts ? renderAccounts(accounts.equity.list) : null }
+            <TableRow>
+              <TableCell component="th" scope="row"><strong>Add : Income & Expenses</strong></TableCell>
+              <TableCell align="right">{(-(accounts.pl_bal)).toFixed(2)}</TableCell>
+            </TableRow>
             <TableRow className={classes.bold}>
               <TableCell component="th" scope="row">Total Equity</TableCell>
-              <TableCell></TableCell>
-              <TableCell align="right">{(-(accounts.equity.total)).toFixed(2)}</TableCell>
+              <TableCell align="right">{(-(accounts.equity.total+accounts.pl_bal)).toFixed(2)}</TableCell>
             </TableRow>
 
             <TableRow className={classes.bold}>
-              <TableCell component="th" scope="row">TOTAL</TableCell>
-              <TableCell></TableCell>
-              <TableCell align="right">{((accounts.f_assets.total+accounts.c_assets.total)+(accounts.n_c_liabilities.total+accounts.c_liabilities.total)+(accounts.equity.total)).toFixed(2)}</TableCell>
+              <TableCell component="th" scope="row"><strong>Total LIABILITIES & EQUITY</strong></TableCell>
+              <TableCell align="right"><strong>{(-(accounts.n_c_liabilities.total+accounts.c_liabilities.total+accounts.equity.total+accounts.pl_bal)).toFixed(2)}</strong></TableCell>
             </TableRow>
           </TableBody>
         </Table>
